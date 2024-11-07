955d925fafc05785355d4e7f398645f1db8b5659
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2024-11-07 14:22:31 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,32 +1,80 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
+const (
+	BUFF_SIZE = 2048
+	CRLF      = "\r\n"
+)
+
+var (
+	ErrNoRequest     = errors.New("No http request provided")
+	ErrInvalidMethod = errors.New("Invalid http method")
+
+	Methods = map[string]struct{}{
+		"GET":    struct{}{},
+		"POST":   struct{}{},
+		"PUT":    struct{}{},
+		"DELETE": struct{}{},
+	}
+)
+
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	//
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+		}
+		fmt.Println("Conn established")
+		go HandleConn(conn)
+	}
+}
+
+func HandleConn(c net.Conn) error {
+	defer c.Close()
+
+	buffer := make([]byte, BUFF_SIZE)
+	nread, err := c.Read(buffer)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return err
+	}
+	lines := strings.Split(string(buffer), CRLF)
+	if nread == 0 || len(lines) < 2 {
+		return ErrNoRequest
 	}
 
-	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+	lines[0] = strings.TrimSpace(lines[0])
+
+	request := strings.Fields(lines[0])
+	// fmt.Println(request[0])
+	// fmt.Println(request[1])
+	// fmt.Println(request[2])
+
+	if _, ok := Methods[request[0]]; !ok {
+		return ErrInvalidMethod
+	}
+
+	if request[1] == "/" {
+		fmt.Fprintf(c, "HTTP/1.1 200 OK\r\n\r\n")
+	} else {
+		fmt.Fprintf(c, "HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
+	return nil
 }
